f0e4e1bd6fc8e7a443d15145df9e727d36db8a45
lzr <3200348589@qq.com>
2023-01-13 17:00:27 +0800

    fzf: add ns3 fuzzy complete function

--- home/lzr/.zsh/fzf.zsh
+++ home/lzr/.zsh/fzf.zsh
@@ -6,3 +6,9 @@ export FZF_COMPLETION_TRIGGER='\'
 
 # export fzf_preview_cmd='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500'
 export FZF_DEFAULT_OPTS='--border --preview "[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500"'
+
+_fzf_complete_ns3() {
+  _fzf_complete --prompt="ns3> " -- "$@" < <(
+    ns3 show targets | sed -n '/Runnable\/Buildable targets:/,$p' | sed '/Runnable\/Buildable targets:/d' | sed 's/ /\n/g' | sed '/^$/d'
+  )
+}
