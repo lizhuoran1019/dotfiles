51042f543f456a79432b6f95cb659c4cb3d56ac1
lzr <3200348589@qq.com>
2023-02-17 16:59:22 +0800

    fzf: show hidden files and skip .git folder

--- home/lzr/.zsh/fzf.zsh
+++ home/lzr/.zsh/fzf.zsh
@@ -1,7 +1,9 @@
 source /usr/share/fzf/key-bindings.zsh
 source /usr/share/fzf/completion.zsh
 
-export FZF_DEFAULT_COMMAND='fd'
+# export FZF_DEFAULT_COMMAND='fd'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --glob '!.git''
+
 export FZF_COMPLETION_TRIGGER='\'
 
 # export fzf_preview_cmd='[[ $(file --mime {}) =~ binary ]] && echo {} is a binary file || (ccat --color=always {} || highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500'
